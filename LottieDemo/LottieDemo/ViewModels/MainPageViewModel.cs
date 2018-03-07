f8458b814c57119a225f8eee3b46f05c1c33b4aa
L3xer <sanya2371@gmail.com>
2018-03-07 09:19:03 +0200

    Update nuget packages.

--- LottieDemo/LottieDemo/ViewModels/MainPageViewModel.cs
+++ LottieDemo/LottieDemo/ViewModels/MainPageViewModel.cs
@@ -1,19 +1,12 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Prism.Navigation;
 
 namespace LottieDemo.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService) 
-            : base (navigationService)
+        public MainPageViewModel(INavigationService navigationService) : base (navigationService)
         {
-            Title = "Main Page";
+            Title = "Lottie Demo";
         }
     }
 }
